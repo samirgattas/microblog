a4bee7585c57949da2e80a68c80251c093eb78aa
samirgattas <samirgattas@gmail.com>
2025-06-20 10:55:48 -0300

    Add user handler dependencies injection

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,24 @@ package main
 
 import (
 	"microblog/internal/adapter/handler/healthcheck"
+	"microblog/internal/adapter/handler/middleware"
+	userhandler "microblog/internal/adapter/handler/user"
+	"microblog/internal/adapter/repository/user"
+	"microblog/internal/core/domain"
+	userservice "microblog/internal/core/service/user"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	usersDB := make(map[int64]domain.User)
+	// Create User repository
+	userRepository := user.NewUserRepository(usersDB)
+	// Create User service
+	userService := userservice.NewUserService(userRepository)
+	// Create User handler
+	userHandler := userhandler.NewUserHandler(userService)
+
 	// Create HealthCheckHandler
 	healthCheckHandler := healthcheck.NewHealthCheckHandler()
 
