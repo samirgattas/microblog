d7a0f4a24944f411d26180b7ae27691e58d43e09
samirgattas <samirgattas@gmail.com>
2025-06-21 10:05:32 -0300

    Add implementation for followed methods

---
+++ internal/adapter/handler/followed/followed.go
@@ -0,0 +1,149 @@
+package followed
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log/slog"
+	"microblog/internal/core/domain"
+	"microblog/internal/core/lib/customerror"
+	"microblog/internal/core/port/handler/followed"
+	followedsrv "microblog/internal/core/port/service/followed"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type followedHandler struct {
+	service followedsrv.FollowedService
+}
+
+func NewFollowedHandler(followedService followedsrv.FollowedService) followed.FollowedHandler {
+	return &followedHandler{
+		service: followedService,
+	}
+}
+
+func (h *followedHandler) CreateFollowed(c *gin.Context) {
+	jsonData, err := io.ReadAll(c.Request.Body)
+	if err != nil {
+		slog.Error("read all error")
+		c.Error(err)
+		return
+	}
+
+	followed := &domain.Followed{}
+	err = json.Unmarshal(jsonData, &followed)
+	if err != nil {
+		slog.Error("followed unmarshal error")
+		c.Error(customerror.NewBadRequestError("invalid request body"))
+		return
+	}
+
+	err = h.service.CreateFollowed(c, followed)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	c.JSON(http.StatusOK, followed)
+}
+
+func (h *followedHandler) GetFollowed(c *gin.Context) {
+	IDStr := c.Param("followed_id")
+	if IDStr == "" {
+		slog.Error("empty followed_id")
+		c.Error(customerror.NewBadRequestError("empty followed_id"))
+		return
+	}
+
+	ID, err := strconv.ParseInt(IDStr, 10, 64)
+	if err != nil {
+		slog.Error(fmt.Sprintf("invalid followed_id: %s", IDStr))
+		c.Error(customerror.NewBadRequestError("invalid followed_id"))
+		return
+	}
+	user, err := h.service.GetFollowed(c, ID)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+	c.JSON(http.StatusOK, user)
+}
+
+func (h *followedHandler) UpdateFollowed(c *gin.Context) {
+	jsonData, err := io.ReadAll(c.Request.Body)
+	if err != nil {
+		slog.Error("read all error")
+		c.Error(err)
+		return
+	}
+
+	followedIDStr := c.Param("followed_id")
+	if followedIDStr == "" {
+		slog.Error("empty followed_id")
+		c.Error(customerror.NewBadRequestError("empty followed_id"))
+		return
+	}
+	followedID, err := strconv.ParseInt(followedIDStr, 10, 64)
+	if err != nil {
+		slog.Error(fmt.Sprintf("invalid followed_id: %s", followedIDStr))
+		c.Error(customerror.NewBadRequestError("invalid followed_id"))
+		return
+	}
+
+	followedPatchCmd := &domain.FollowedPatchCommand{}
+	err = json.Unmarshal(jsonData, &followedPatchCmd)
+	if err != nil {
+		slog.Error("followed unmarshal error")
+		c.Error(customerror.NewBadRequestError("invalid request body"))
+		return
+	}
+
+	followed, err := h.service.UpdateFollowed(c, followedID, followedPatchCmd)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	c.JSON(http.StatusOK, followed)
+}
+
+func (h *followedHandler) SearchFollowed(c *gin.Context) {
+	var userID *int64
+	userIDStr := c.Query("user_id")
+	if userIDStr != "" {
+		userIDTmp, err := strconv.ParseInt(userIDStr, 10, 64)
+		if err != nil {
+			slog.Error(fmt.Sprintf("invalid user_id: %s", userIDStr))
+			c.Error(customerror.NewBadRequestError("invalid user_id"))
+			return
+		}
+		userID = &userIDTmp
+	}
+
+	var followedUserID *int64
+	followedUserIDStr := c.Query("followed_user_id")
+	if followedUserIDStr != "" {
+		followedUserIDTmp, err := strconv.ParseInt(followedUserIDStr, 10, 64)
+		if err != nil {
+			slog.Error(fmt.Sprintf("invalid user_id: %s", followedUserIDStr))
+			c.Error(customerror.NewBadRequestError("invalid user_id"))
+			return
+		}
+		followedUserID = &followedUserIDTmp
+	}
+
+	if userID == nil && followedUserID == nil {
+		c.Error(customerror.NewBadRequestError("invalid user_id"))
+		return
+	}
+
+	followed, err := h.service.SearchFollowed(c, userID, followedUserID)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"total": len(followed), "followed": followed})
+}
---
+++ internal/core/service/followed/followed.go
@@ -0,0 +1,99 @@
+package followed
+
+import (
+	"context"
+	"microblog/internal/core/domain"
+	"microblog/internal/core/lib/customerror"
+	"microblog/internal/core/port/repository"
+	"microblog/internal/core/port/service/followed"
+)
+
+type followedService struct {
+	repository     repository.FollowedRepository
+	userRepository repository.UserRepository
+}
+
+func NewFollowedService(followedRepository repository.FollowedRepository, userRepository repository.UserRepository) followed.FollowedService {
+	return &followedService{
+		repository:     followedRepository,
+		userRepository: userRepository,
+	}
+}
+
+func (s *followedService) CreateFollowed(ctx context.Context, followed *domain.Followed) error {
+	// Check if the user id exists
+	_, err := s.userRepository.Get(followed.UserID)
+	if err != nil {
+		return err
+	}
+
+	// Check if the followed user id exists
+	_, err = s.userRepository.Get(followed.FollowedUserID)
+	if err != nil {
+		return err
+	}
+
+	// Set enabled to true because the creation occurs when the user_id starts following the followed_user_id
+	followed.Enabled = true
+
+	// Save the followed entity
+	err = s.repository.Save(ctx, followed)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *followedService) GetFollowed(ctx context.Context, followedID int64) (*domain.Followed, error) {
+	// Get the followed entity
+	followed, err := s.repository.Get(ctx, followedID)
+	if err != nil {
+		return &domain.Followed{}, err
+	}
+
+	return followed, nil
+}
+
+func (s *followedService) UpdateFollowed(ctx context.Context, followedID int64, followedPatchCmd *domain.FollowedPatchCommand) (*domain.Followed, error) {
+	// Check if enabled is set
+	if followedPatchCmd.Enabled == nil {
+		return &domain.Followed{}, customerror.NewBadRequestError("enabled should not be null")
+	}
+
+	// Get the followed entity
+	followed, err := s.repository.Get(ctx, followedID)
+	if err != nil {
+		return &domain.Followed{}, err
+	}
+
+	// If the enabled field is not modified, then return
+	if followed.Enabled == *followedPatchCmd.Enabled {
+		return followed, nil
+	}
+
+	// Set the enabled field
+	followed.Enabled = *followedPatchCmd.Enabled
+
+	// Update followed
+	err = s.repository.Update(ctx, followed)
+	if err != nil {
+		return &domain.Followed{}, err
+	}
+
+	return followed, nil
+}
+
+func (s *followedService) SearchFollowed(ctx context.Context, userID *int64, followedUserID *int64) ([]domain.Followed, error) {
+	// Get the followed entity
+	followed, err := s.repository.SearchByUserIDAndFollowedUserID(ctx, userID, followedUserID)
+	if err != nil {
+		return []domain.Followed{}, err
+	}
+
+	// Followed does not exist
+	if len(followed) == 0 {
+		return []domain.Followed{}, customerror.NewNotFoundError("followed")
+	}
+
+	return followed, nil
+}
