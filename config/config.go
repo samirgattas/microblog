2dd51838e32a67ae7710ec0ec3273ff717681bce
samirgattas <samirgattas@gmail.com>
2025-06-29 17:06:45 -0300

    Modify change struct. It only has app settings

--- config/config.go
+++ config/config.go
@@ -2,38 +2,35 @@ package config
 
 import (
 	"os"
-
-	"github.com/samirgattas/microblog/internal/core/domain"
-	inmemorystore "github.com/samirgattas/microblog/lib/in_memory_store"
 )
 
 var c *Config
 
 type Config struct {
-	UsersDB    inmemorystore.Store
-	FollowedDB map[int64]domain.Followed
-	TweetDB    map[int64]domain.Tweet
-	Domain     string
-	PortDB     int64
-	DomainDB   string
-	UserDB     string
-	PasswordDB string
+	MySQL  Database `yaml:"mysql"`
+	Domain string   `yaml:"domain"`
 }
 
-func (c *Config) NewConfig(userDB inmemorystore.Store, followedDB map[int64]domain.Followed, tweetDB map[int64]domain.Tweet) *Config {
+type Database struct {
+	Domain   string `yaml:"domain"`
+	Port     int64  `yaml:"port"`
+	User     string `yaml:"user"`
+	Password string `yaml:"password"`
+}
+
+func (c *Config) NewConfig() *Config {
 	domain := "localhost"
 	if isDockerEnv() {
 		domain = ""
 	}
 	c = &Config{
-		UsersDB:    userDB,
-		FollowedDB: followedDB,
-		TweetDB:    tweetDB,
-		Domain:     domain,
-		PortDB:     3307,
-		DomainDB:   "127.0.0.1",
-		UserDB:     "root",
-		PasswordDB: "root",
+		Domain: domain,
+		MySQL: Database{
+			Port:     3307,
+			Domain:   "127.0.0.1",
+			User:     "root",
+			Password: "root",
+		},
 	}
 	return c
 }
